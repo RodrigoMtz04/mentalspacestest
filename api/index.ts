e415db6d435ab2bbb7c5cc21fcd0242638bbc8f8
RodrigoMtz04 <r1199785@uabc.edu.mx>
2025-11-25 19:21:09 -0800

    aaaaaa

--- api/index.ts
+++ api/index.ts
@@ -17,3 +17,5 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   const handler = await getHandler();
   return handler(req as any, res as any);
 }
+
+export { default } from "./handler";
